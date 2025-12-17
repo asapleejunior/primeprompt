9484a8cb10e23ff0eb03e78840ada827eea14242
Valencio <150917016+asapleejunior@users.noreply.github.com>
2025-12-18 00:16:59 +0300

    Figma integration

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const handleMobileMenu = () => {
+    const navMenuMobile = document.getElementById("navMobileMenu");
+    const mobileMenu = document.getElementById("mobileMenu");
+    const mobileMenuClose = document.getElementById("mobileMenuClose");
+
+    const openMenu = () => {
+        mobileMenu.classList.add("mobile-menu--open");
+        document.body.classList.add("no-scroll");
+    };
+
+    const closeMenu = () => {
+        mobileMenu.classList.remove("mobile-menu--open");
+        document.body.classList.remove("no-scroll");
+    };
+
+    navMenuMobile.addEventListener("click", openMenu);
+    mobileMenuClose.addEventListener("click", closeMenu);
+};
+
+const animateMainContent = () => {
+    const elementsToAnimate = [
+        { selector: ".main__hero-title", transform: false },
+        { selector: ".main__hero-description", transform: false },
+        { selector: ".main__hero-mobile-description", transform: false },
+        { selector: ".main__hero-button", transform: false },
+        ...Array.from(document.querySelectorAll(".main__feature")).map((feature, index) => ({
+            element: feature,
+            transform: true
+        }))
+    ];
+
+    const animateElement = (element, delay = 0, transform = false) => {
+        element.style.opacity = 0;
+        if (transform) {
+            element.style.transform = "translateY(20px)";
+        }
+        setTimeout(() => {
+            element.style.transition = "opacity 0.6s ease-in-out, transform 0.6s ease-out";
+            element.style.opacity = 1;
+            if (transform) {
+                element.style.transform = "translateY(0)";
+            }
+        }, delay);
+    };
+
+    elementsToAnimate.forEach((item, index) => {
+        const element = item.element || document.querySelector(item.selector);
+        if (element) {
+            animateElement(element, index * 200, item.transform);
+        }
+    });
+};
+
+handleMobileMenu();
+animateMainContent();
